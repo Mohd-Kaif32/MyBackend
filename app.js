da7ab32a31def101355d4cdc9fdb332a8b69c111
Mohd-Kaif32 <mohdkaifjhanda466@gmail.com>
2025-09-05 23:21:37 +0530

    second commit

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
-const express=require("express");
-const app=express();
-const cookieParser=require("cookie-parser");
-const bodyParser=require("body-parser");
-const fileUpload=require("express-fileupload");
-const cors=require("cors");
+// const express=require("express");
+// const app=express();
+// const cookieParser=require("cookie-parser");
+// const bodyParser=require("body-parser");
+// const fileUpload=require("express-fileupload");
+// const cors=require("cors");
 
-const errorMiddleware=require("./middleware/error.js");
+// const errorMiddleware=require("./middleware/error.js");
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   })
+// );
 
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(fileUpload());
+
+// const product=require("./routes/productRoute");
+// const user=require("./routes/userRoute.js");
+// const order=require("./routes/orderRoutes.js");
+
+// app.use("/api/v1",product);
+// app.use("/api/v1",user);
+// app.use("/api/v1",order);
+
+// app.use(errorMiddleware)
+
+
+
+// module.exports=app
+
+const express = require('express');
+const app = express();
+
+// middlewares
 app.use(express.json());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(fileUpload());
 
-const product=require("./routes/productRoute");
-const user=require("./routes/userRoute.js");
-const order=require("./routes/orderRoutes.js");
+// routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
-app.use("/api/v1",product);
-app.use("/api/v1",user);
-app.use("/api/v1",order);
-
-app.use(errorMiddleware)
-
-
-
-module.exports=app
-
+// Export the app (do NOT call listen here)
+module.exports = app;
